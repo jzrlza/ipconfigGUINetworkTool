10471bf4655dffdda20d3a7e059067cf88913244
jzrlza <parinvut12345@gmail.com>
2017-11-18 13:16:18 +0700

    Added unix base OS display ability.

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,15 +1,6 @@
 import java.awt.EventQueue;
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.text.ParseException;
-import java.util.StringTokenizer;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -52,71 +43,117 @@ public class MainWindow {
 	public MainWindow() {
 		initialize();
 		refresh();
-		
-		
-	}
-	
-	 public void refresh() {
-		 String[] a = executeCommand();
-			boolean connected = false;
-			if(System.getProperty("os.name").contains("Window")) {
-				int wifiIndex = 0;
-				for(String x : a) {
-					if(x.contains("Wireless LAN adapter Wi-Fi")) {
-						break;
-					}
-					//System.out.println(x);
-					
-					wifiIndex++;
-				}
-				if(!a[wifiIndex+2].contains("Media State . . . . . . . . . . . : Media disconnected")) {
-					connected = true;
-				}
-				if(connected) {
-					for(int i = wifiIndex; i < wifiIndex+26; i++) {
-						if(a[i].contains("Connection-specific DNS Suffix  . :")) {
-							dnsSuffix = a[i].substring(39, a[i].length());
-						}
-						if(a[i].contains("IPv6 Address. . . . . . . . . . . :")) {
-							ipv6 = a[i].substring(39, a[i].length());
-						}
-						if(a[i].contains("IPv4 Address. . . . . . . . . . . :")) {
-							ipv4 = a[i].substring(39, a[i].length());
-						}
-						if(a[i].contains("Subnet Mask . . . . . . . . . . . :")) {
-							subnetMask = a[i].substring(39, a[i].length());
-						}
-						if(a[i].contains("Physical Address. . . . . . . . . :")) {
-							macAd = a[i].substring(39, a[i].length());
-						}
-					}
-					connectedness = "Connected";
-				} else {
-					for(int i = wifiIndex; i < wifiIndex+7; i++) {
-						if(a[i].contains("Connection-specific DNS Suffix  . :")) {
-							dnsSuffix = a[i].substring(39, a[i].length());
-						}
-						if(a[i].contains("Physical Address. . . . . . . . . :")) {
-							macAd = a[i].substring(39, a[i].length());
-						}
-					}
-					ipv6 = "Cannot Currently Display, Media Disconnected";
-					ipv4 = "Cannot Currently Display, Media Disconnected";
-					subnetMask = "Cannot Currently Display, Media Disconnected";
-					connectedness = "Disconnected";
-				}
-				
-			}
-			
 
-			dnsSuffixText.setText(dnsSuffix);
-			ipv6Text.setText(ipv6);
-			ipv4Text.setText(ipv4);
-			snMaskText.setText(subnetMask);
-			macAdText.setText(macAd);
-			connectionStatusText.setText(connectedness);
-	 }
-	
+
+	}
+
+	public void refresh() {
+		String[] a = executeCommand();
+		boolean connected = false;
+
+		if(System.getProperty("os.name").contains("Window")) {
+			int wifiIndex = 0;
+			for(String x : a) {
+				if(x.contains("Wireless LAN adapter Wi-Fi")) {
+					break;
+				}
+				//System.out.println(x);
+
+				wifiIndex++;
+			}
+			if(!a[wifiIndex+2].contains("Media State . . . . . . . . . . . : Media disconnected")) {
+				connected = true;
+			}
+			if(connected) {
+				for(int i = wifiIndex; i < wifiIndex+26; i++) {
+					if(a[i].contains("Connection-specific DNS Suffix  . :")) {
+						dnsSuffix = a[i].substring(39, a[i].length());
+					}
+					if(a[i].contains("IPv6 Address. . . . . . . . . . . :")) {
+						ipv6 = a[i].substring(39, a[i].length());
+					}
+					if(a[i].contains("IPv4 Address. . . . . . . . . . . :")) {
+						ipv4 = a[i].substring(39, a[i].length());
+					}
+					if(a[i].contains("Subnet Mask . . . . . . . . . . . :")) {
+						subnetMask = a[i].substring(39, a[i].length());
+					}
+					if(a[i].contains("Physical Address. . . . . . . . . :")) {
+						macAd = a[i].substring(39, a[i].length());
+					}
+				}
+				connectedness = "Connected";
+			} else {
+				for(int i = wifiIndex; i < wifiIndex+7; i++) {
+					if(a[i].contains("Connection-specific DNS Suffix  . :")) {
+						dnsSuffix = a[i].substring(39, a[i].length());
+					}
+					if(a[i].contains("Physical Address. . . . . . . . . :")) {
+						macAd = a[i].substring(39, a[i].length());
+					}
+				}
+				ipv6 = "Cannot Currently Display, Media Disconnected";
+				ipv4 = "Cannot Currently Display, Media Disconnected";
+				subnetMask = "Cannot Currently Display, Media Disconnected";
+				connectedness = "Disconnected";
+			}
+		}
+		
+		//for unix base os.
+		else {
+			String[] collectedData;
+			String line = "";
+			int index = 0;
+			for(String x : a) {
+				if(x.contains("en0: flags=")) {
+					for(int i=index;i<index+7;i++) {
+						if(a[i].contains("en1: flags=")) {
+							break;
+						}
+						line += a[i]+" -split_here- ";
+						if(a[i].contains("status: active")) {
+							connected = true;
+						}
+					}
+				}
+				if(x.contains("domain ")) {
+					line += x;
+					break;
+				}
+				index++;
+			}
+			collectedData = line.split(" -split_here- ");
+			for(String x : collectedData) {
+				System.out.println(x);
+			}
+			macAd = collectedData[1].substring(6);
+			if(connected) {
+				int endOfIpv6 = collectedData[2].indexOf("prefixlen");
+				int endOfIpv = collectedData[3].indexOf("netmask");
+				int endOfNet = collectedData[3].indexOf("broadcast");
+				ipv6 = collectedData[2].substring(6,endOfIpv6);
+				ipv4 = collectedData[3].substring(5,endOfIpv);
+				dnsSuffix = collectedData[7].substring(7);
+				subnetMask = collectedData[3].substring(endOfIpv+7,endOfNet);
+				connectedness = "Connected";
+			}
+			else {
+				dnsSuffix = "Cannot Currently Display, Media Disconnected";
+				ipv6 = "Cannot Currently Display, Media Disconnected";
+				ipv4 = "Cannot Currently Display, Media Disconnected";
+				subnetMask = "Cannot Currently Display, Media Disconnected";
+				connectedness = "Disconnected";
+			}
+		}
+
+		dnsSuffixText.setText(dnsSuffix);
+		ipv6Text.setText(ipv6);
+		ipv4Text.setText(ipv4);
+		snMaskText.setText(subnetMask);
+		macAdText.setText(macAd);
+		connectionStatusText.setText(connectedness);
+	}
+
 
 	/**
 	 * Initialize the contents of the frame.
@@ -128,67 +165,67 @@ public class MainWindow {
 		frmIpConfigurationFor.setBounds(10, 10, 555, 454);
 		frmIpConfigurationFor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmIpConfigurationFor.getContentPane().setLayout(null);
-		
+
 		JLabel lblConnectionStatus = new JLabel("Connection Status: ");
 		lblConnectionStatus.setBounds(27, 29, 179, 16);
 		frmIpConfigurationFor.getContentPane().add(lblConnectionStatus);
-		
+
 		JLabel lblIpv = new JLabel("IPv4");
 		lblIpv.setBounds(27, 86, 112, 16);
 		frmIpConfigurationFor.getContentPane().add(lblIpv);
-		
+
 		JLabel lblIpv_1 = new JLabel("IPv6");
 		lblIpv_1.setBounds(27, 132, 112, 16);
 		frmIpConfigurationFor.getContentPane().add(lblIpv_1);
-		
+
 		JLabel lblDnsSuffix = new JLabel("DNS Suffix");
 		lblDnsSuffix.setBounds(27, 183, 112, 16);
 		frmIpConfigurationFor.getContentPane().add(lblDnsSuffix);
-		
+
 		JLabel lblMacAddress = new JLabel("MAC Address");
 		lblMacAddress.setBounds(27, 236, 112, 16);
 		frmIpConfigurationFor.getContentPane().add(lblMacAddress);
-		
+
 		JLabel lblSubnetMask = new JLabel("Subnet Mask");
 		lblSubnetMask.setBounds(27, 287, 112, 16);
 		frmIpConfigurationFor.getContentPane().add(lblSubnetMask);
-		
+
 		connectionStatusText = new JTextField();
 		connectionStatusText.setEditable(false);
 		connectionStatusText.setBounds(208, 26, 310, 22);
 		frmIpConfigurationFor.getContentPane().add(connectionStatusText);
 		connectionStatusText.setColumns(10);
-		
+
 		ipv4Text = new JTextField();
 		ipv4Text.setEditable(false);
 		ipv4Text.setBounds(208, 83, 310, 22);
 		frmIpConfigurationFor.getContentPane().add(ipv4Text);
 		ipv4Text.setColumns(10);
-		
+
 		ipv6Text = new JTextField();
 		ipv6Text.setEditable(false);
 		ipv6Text.setBounds(208, 129, 310, 22);
 		frmIpConfigurationFor.getContentPane().add(ipv6Text);
 		ipv6Text.setColumns(10);
-		
+
 		dnsSuffixText = new JTextField();
 		dnsSuffixText.setEditable(false);
 		dnsSuffixText.setBounds(208, 180, 310, 22);
 		frmIpConfigurationFor.getContentPane().add(dnsSuffixText);
 		dnsSuffixText.setColumns(10);
-		
+
 		macAdText = new JTextField();
 		macAdText.setEditable(false);
 		macAdText.setBounds(208, 233, 310, 22);
 		frmIpConfigurationFor.getContentPane().add(macAdText);
 		macAdText.setColumns(10);
-		
+
 		snMaskText = new JTextField();
 		snMaskText.setEditable(false);
 		snMaskText.setBounds(208, 284, 310, 22);
 		frmIpConfigurationFor.getContentPane().add(snMaskText);
 		snMaskText.setColumns(10);
-		
+
 		JButton btnNewButton = new JButton("Refresh");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -198,9 +235,9 @@ public class MainWindow {
 		btnNewButton.setBounds(12, 328, 512, 78);
 		frmIpConfigurationFor.getContentPane().add(btnNewButton);
 	}
-	
-	
-	
+
+
+
 	private String[] executeCommand() {
 		Process command;
 		try {
@@ -217,34 +254,20 @@ public class MainWindow {
 			while ((line = buf.readLine()) != null) {
 				output += line + "split_here";
 			}
+			
+			if(!System.getProperty("os.name").contains("Window")) {
+				command = Runtime.getRuntime().exec("cat /etc/resolv.conf");
+				buf = new BufferedReader(new InputStreamReader(command.getInputStream()));
+				while ((line = buf.readLine()) != null) {
+					output += line + "split_here";
+				}
+			}//to get DNS of unix base os.
+
 			String[] a = output.split("split_here");
-			//for(String x : a) {
-				//if(x.contains("Wi-Fi"))
-			//	System.out.println(x);
-			//}
+			
+			
 			return a;
 
-			/*
-			 * This code below is for collecting data like ipv4 , 6 and etc. but not done yet.
-			 */
-
-			//			int check = 0,loca = 1;
-			//			String[] info = new String[6];
-			//			for(String x : a) {
-			//				System.out.println(x);
-			//				if(x.contains("en0: ")) {
-			//					check = 1;
-			//				}
-			//				if(check == 1) {
-			//					if(x.contains("inet")) {
-			//						info[loca] = x;
-			//						loca++;
-			//					}
-			//				}
-			//			}
-			//			System.out.println("==============");
-			//			System.out.println(info[1]);
-			//			System.out.println(info[2]);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
